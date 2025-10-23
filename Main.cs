db00f7a7f13f0d35c7f2c02d35a919bd1c6ac701
Dilon Sjamsuddin <130769530+wasdeilonn@users.noreply.github.com>
2025-10-23 20:58:07 +0200

    added shit

--- Main.cs
+++ Main.cs
@@ -1,10 +1,14 @@
 using BepInEx.Logging;
+using HarmonyLib;
 
 namespace PolyScriptTemplate;
 public static class Main
 {
+    public static ManualLogSource? modLogger;
     public static void Load(ManualLogSource logger)
     {
-        logger.LogMessage("Here we go!");
+        modLogger = logger;
+        modLogger.LogMessage("Here we go!");
+        Harmony.CreateAndPatchAll(typeof(Main));
     }
 }
